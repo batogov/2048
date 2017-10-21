492c6ad8103aca528e25ade7a342d22d443c43c7
Dmitriy Batogov <hi@batogov.ru>
2017-10-21 19:19:56 +0300

    Добавил иконку и лоадер для svg

--- webpack/development.config.js
+++ webpack/development.config.js
@@ -20,6 +20,10 @@ module.exports = merge([{
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: 'babel-loader'
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader?removeSVGTagAttrs=false'
             }
         ]
     },
